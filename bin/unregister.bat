3ec9cf4d9645bbc54ad1cfc2b86fddf440bc1e0b
Fanxiushu <fanxiushu@163.com>
2018-04-06 02:01:30 +0800

    xfsredir files system uploaded

---
+++ bin/unregister.bat
@@ -0,0 +1,7 @@
+@echo off
+mode con lines=30 cols=60
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+rem ----
+
+regsvr32 /u vcam_dshow.dll
